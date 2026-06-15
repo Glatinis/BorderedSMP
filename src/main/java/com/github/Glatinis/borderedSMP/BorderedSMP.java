2edd70a063038b727213ffebadebee634002a410
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-06-15 12:45:44 +0800

    Initial commit

---
+++ src/main/java/com/github/Glatinis/borderedSMP/BorderedSMP.java
@@ -0,0 +1,16 @@
+package com.github.Glatinis.borderedSMP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BorderedSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
